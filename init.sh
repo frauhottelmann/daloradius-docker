4ccf9044af348dd89a25597f8316458481011cbc
frauhottelmann <frauhottelmann@users.noreply.github.com>
2020-05-11 18:56:51 +0200

    disable mysqlsql encryption

--- init.sh
+++ init.sh
@@ -19,6 +19,10 @@ mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < 
 sed -i 's|driver = "rlm_sql_null"|driver = "rlm_sql_mysql"|' /etc/freeradius/3.0/mods-available/sql 
 sed -i 's|dialect = "sqlite"|dialect = "mysql"|' /etc/freeradius/3.0/mods-available/sql
 sed -i 's|dialect = ${modules.sql.dialect}|dialect = "mysql"|' /etc/freeradius/3.0/mods-available/sqlcounter # avoid instantiation error
+sed -i 's|ca_file = "/etc/ssl/certs/my_ca.crt"|#ca_file = "/etc/ssl/certs/my_ca.crt"|' /etc/freeradius/3.0/mods-available/sql #disable sql encryption
+sed -i 's|certificate_file = "/etc/ssl/certs/private/client.crt"|#certificate_file = "/etc/ssl/certs/private/client.crt"|' /etc/freeradius/3.0/mods-available/sql #disable sql encryption
+sed -i 's|private_key_file = "/etc/ssl/certs/private/client.key"|#private_key_file = "/etc/ssl/certs/private/client.key"|' /etc/freeradius/3.0/mods-available/sql #disable sql encryption
+sed -i 's|tls_required = yes|tls_required = no|' /etc/freeradius/3.0/mods-available/sql #disable sql encryption
 sed -i 's|#\s*read_clients = yes|read_clients = yes|' /etc/freeradius/3.0/mods-available/sql 
 ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/sql
 ln -s /etc/freeradius/3.0/mods-available/sqlcounter /etc/freeradius/3.0/mods-enabled/sqlcounter
